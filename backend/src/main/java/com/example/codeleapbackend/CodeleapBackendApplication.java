b9fcc06ee4a73fa6f3365eedcb070f9a611bb243
Icsya05 <2797066299@qq.com>
2026-04-26 16:59:20 +0800

    feat(backend): 初始化SpringBoot后端基础框架

---
+++ backend/src/main/java/com/example/codeleapbackend/CodeleapBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.codeleapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeleapBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeleapBackendApplication.class, args);
+    }
+}
