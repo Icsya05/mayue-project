3060acd74de961299c366d59d8de8585b95a7921
qwertyu604 <2557027275@qq.com>
2026-05-17 15:56:17 +0800

    登陆注册

---
+++ backend/src/main/java/com/example/codeleapbackend/user/pojo/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.codeleapbackend.user.pojo.entity;
+
+import lombok.Data;
+@Data
+public class User {
+    private int id;
+    private String username;
+    private String password;
+}
